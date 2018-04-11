e6fd05fa8455bae0abb150ee1549a60a774467f7
smosc <oscar@sm-eng.co.uk>
2018-04-11 22:55:41 +0100

    Update expStr.js

--- expStr/expStr.js
+++ expStr/expStr.js
@@ -1,12 +1,18 @@
-var expStr = function(i,z){
-	i.match(/.-./g).forEach(function(x,y){
-		i=i.replace(x,"");
-		for(y=x.charCodeAt(0);y<=x.charCodeAt(2);y++){
-			z=(z?z:"")+String.fromCharCode(y);
-        }
-    });
-	i.split("").forEach(function(x){
-		z+=(!z.match(new RegExp(x.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),"g"))?x:"");
-    });
+var expStr = function(i, z)
+{
+	// Find x-X matches, remove from input string, and add all chars inbetween to output
+	i.match(/.-./g).forEach(function(x, y)
+	{
+		for (y = x.charCodeAt(0); y <= x.charCodeAt(2); y++)
+		{
+			z = (z ? z : "") + String.fromCharCode(y);
+			i = i.replace(x, "");
+		}
+	});
+	// Cycle through remaining chars in input string, add if string doesn't already contain
+	i.split("").forEach(function(x)
+	{
+		z += (z.indexOf(x)===-1 ? x : "");
+	});
 	return z;
 };
